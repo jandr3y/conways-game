2d1aeb47770092e1dfd7b74d3535cb6cf917fff0
Lucas Jandrey <wdev.lucas@gmail.com>
2024-08-01 22:50:19 -0300

    trying to improve performance

--- script.js
+++ script.js
@@ -14,34 +14,38 @@ const ACORN_CELLS = new Set([ '55,55', '56,55', '56,53', '58,54', '59,55', '60,5
 let ALIVE_CELLS = new Set([...ACORN_CELLS]); 
 
 const pause = () => paused = !paused
-const reset = () => ALIVE_CELLS = new Set([...ACORN_CELLS])
+const reset = () => {
+  ALIVE_CELLS = new Set([...ACORN_CELLS])
+  gen = 0
+}
 
 function findAliveParents(cell, aliveCells) {
   const parents = new Set()
-  aliveCells.forEach(cellFind => {
-    const cf = cellFind.split(',')
-    const c = cell.split(',')
+  const c = cell.split(',').map(Number)
 
+  for (const cellFind of aliveCells) {
+    const cf = cellFind.split(',').map(Number)
+  
     if (
-      cf[0] >= (Number(c[0]) - 1) &&
-      cf[0] <= (Number(c[0]) + 1) &&
+      cf[0] >= (c[0] - 1) &&
+      cf[0] <= (c[0] + 1) &&
       cf[1] >= (c[1] - 1) &&
-      cf[1] <= (Number(c[1]) + 1) &&
+      cf[1] <= (c[1] + 1) &&
       cell !== cellFind
     ) {
       parents.add(cellFind)
     }
-  })
+  }
 
   return parents
 }
 
 function findDeadParents(cell, aliveParents) {
   const deadCells = new Set();
-  const c = cell.split(',')
+  const c = cell.split(',').map(Number)
 
-  for (let i = (c[0] - 1); i <= (Number(c[0]) + 1); i++) {
-    for (let j = (c[1] - 1); j <= (Number(c[1]) + 1); j++) {
+  for (let i = (c[0] - 1); i <= (c[0] + 1); i++) {
+    for (let j = (c[1] - 1); j <= (c[1] + 1); j++) {
       if (!aliveParents.has(`${i},${j}`)) {
         deadCells.add(`${i},${j}`)
       }
@@ -59,19 +63,14 @@ function calc() {
     const aliveParents = findAliveParents(cell, ALIVE_CELLS)
     const deadParents  = findDeadParents(cell, aliveParents)
     
-    deadParents.forEach(deadCell => {
+    for (const deadCell of deadParents) {
       const deadCellAliveParents = findAliveParents(deadCell, ALIVE_CELLS)
       if (deadCellAliveParents.size === 3) {
         cellsToCreate.push(deadCell)
       }
-    })
-
-    if (aliveParents.size < 2) {
-      cellsToKill.push(cell)
-      return
     }
-  
-    if (aliveParents.size > 3) {
+
+    if (aliveParents.size < 2 || aliveParents.size > 3) {
       cellsToKill.push(cell)
       return
     }
